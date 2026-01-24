8a12df07bd866d072d624cf8667665b598823d1a
Robert Respicio <respicio.dev+github@gmail.com>
2026-01-23 16:16:31 -0800

    added the nav-bar

--- lib/server-actions/jobApplications.ts
+++ lib/server-actions/jobApplications.ts
@@ -1,11 +1,23 @@
 "use server";
 
-// import JobApplication from "../models/jobApplications"
+import dbConnect from "../db/mongo";
+import JobApplication from "../models/jobApplications"
+import { notFound } from "next/navigation";
 
 export async function getAllJobApplications() {
+    await dbConnect();
   try {
     console.log("TODO: get all job applications");
+    const jobApplications = await JobApplication.find();
+    return jobApplications;
   } catch (error) {
     console.error("TODO: error", error);
+    notFound();
   }
 }
+
+// NOTES 
+
+// the job app tracking portion, with lanes and cards
+// eventually a table
+// if no jobs, display a no jobs tracked
